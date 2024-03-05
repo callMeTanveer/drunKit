5f78b4d4c3e4fb117faf9dcdefd09a1c36434083
M Tanveer <mohammadtanveer7540@gmail.com>
2024-03-05 19:25:05 +0530

    .

---
+++ index.js
@@ -0,0 +1,89 @@
+const sound = new Audio(); 
+
+drumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < drumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", operation)
+}
+
+function operation() {
+    let btn = this.innerText; 
+    if(btn === "w"){
+        sound.src = "./sounds/crash.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "a"){
+        sound.src = "./sounds/kick-bass.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "s"){
+        sound.src = "./sounds/snare.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "d"){
+        sound.src = "./sounds/tom-1.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "j"){
+        sound.src = "./sounds/tom-2.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "k"){
+        sound.src = "./sounds/tom-3.mp3"; 
+        sound.play(); 
+    }
+
+    else if (btn === "l"){
+        sound.src = "./sounds/tom-4.mp3"; 
+        sound.play(); 
+    }
+
+    else {
+        alert("Somethng went wrong!!")
+    }
+}
+
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key); 
+}); 
+
+function makeSound(btn){
+    if(btn === "w"){
+        sound.src = "./sounds/crash.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "a"){
+        sound.src = "./sounds/kick-bass.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "s"){
+        sound.src = "./sounds/snare.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "d"){
+        sound.src = "./sounds/tom-1.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "j"){
+        sound.src = "./sounds/tom-2.mp3"; 
+        sound.play(); 
+    }
+
+    else if(btn === "k"){
+        sound.src = "./sounds/tom-3.mp3"; 
+        sound.play(); 
+    }
+
+    else if (btn === "l"){
+        sound.src = "./sounds/tom-4.mp3"; 
+        sound.play(); 
+    }
+}
